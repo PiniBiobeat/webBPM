8b4e4dd191b5581b6ec2061421cdefe3545036b7
pinimlupa <pinim@lupa.co.il>
2022-08-31 14:15:04 +0300

    test
    test

--- infra/http_util.py
+++ infra/http_util.py
@@ -12,15 +12,15 @@ def http_request(sla_name, url: str, method, headers=None, auth=None):
     session = requests.session()
     session.auth = auth
     response = None
-    # match method:
-    #     case 'get':
-    #         response = session.get(url, headers=headers)
-    #     case 'post':
-    #         response = session.post(url, headers=headers)
-    #     case 'put':
-    #         response = session.put(url, headers=headers)
-    #     case 'delete':
-    #         response = session.delete(url, headers=headers)
+    match method:
+        case 'get':
+            response = session.get(url, headers=headers)
+        case 'post':
+            response = session.post(url, headers=headers)
+        case 'put':
+            response = session.put(url, headers=headers)
+        case 'delete':
+            response = session.delete(url, headers=headers)
 
     end = datetime.datetime.now()
     sla = int((end - start).total_seconds() * 1000)
