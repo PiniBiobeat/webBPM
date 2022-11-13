7e93dfb5030082841fa4c71b49948cc85a40c94d
pini <pinim@lupa.co.il>
2022-11-13 11:46:30 +0200

    add coupon
    add coupon

---
+++ Check_Coupon_Automation/fix_coupon_per.py
@@ -0,0 +1,18 @@
+import pyodbc
+
+def test_connect_to_db():
+    server = '104.155.49.95'
+    database = 'lupa'
+    username = 'MachineDBA'
+    password = 'Kk28!32Zx'
+    # ENCRYPT defaults to yes starting in ODBC Driver 18. It's good to always specify ENCRYPT=yes on the client side to avoid MITM attacks.
+    cnxn = pyodbc.connect(
+        'DRIVER={ODBC Driver 17 for SQL Server};SERVER=' + server + ';DATABASE=' + database + ';Encrypt = Optional;UID=' + username + ';PWD=' + password)
+
+
+    cursor = cnxn.cursor()
+    print(cursor)
+    a = cursor.execute("EXEC [lupa].[dbo].[delete_expired_coupons]")
+    cursor.commit()
+    print(a)
+    cursor.close()
