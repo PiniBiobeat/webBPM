b9c6880b1330adef59f50ee14f3641f234f4fb44
pini <pinim@lupa.co.il>
2023-01-08 13:56:47 +0200

    add_code8.1.23
    add_code8.1.23

--- Check_Coupon_Automation/check_v3_isValid.py
+++ Check_Coupon_Automation/check_v3_isValid.py
@@ -18,7 +18,7 @@ def create_token():
     return  token
 
 
-def test_check_all_site_ver3():
+def test_check_all_site_ver3_is_valid():
 
     class Error:
         def __init__(self, url, siteName):
@@ -41,15 +41,17 @@ def test_check_all_site_ver3():
         print("You are connected to - ", record, "\n")
         list_url_err = list()
         token_from_login = create_token()
-        event_token = "&event_token=f932c5f9a2914232b085724ac6b39dbd"
         for i in record:
             token_site = i[0]
-            response = requests.get(token_site + token_from_login)
-            if response.status_code != 200:
-                e = {"source": "AutomationMonitor", "service_api": i[1], "error_code": 17, "active": "true",
-                     "token": "", "extra_params": {"url": i[0]}}
-                list_url_err.append(e)
-                continue
+            if i[3] == '1' :
+                response = requests.get(token_site + token_from_login)
+                token_value = response.json()
+                if token_value['isValid'] != True:
+
+                    e = {"source": "AutomationMonitor_isValid", "service_api": i[1], "error_code": 17, "active": "true",
+                         "token": "", "extra_params": {"url": i[0],"error": token_value['Error']}}
+                    list_url_err.append(e)
+                    continue
         token_url = 'https://monitor.lupa.co.il/api/api.aspx?method=write_errors&source=AutomationMonitor&service_api=hi'
         obj = {}
         obj["errors"] = str(list_url_err)
@@ -63,7 +65,5 @@ def test_check_all_site_ver3():
             cursor.close()
             connection.close()
             print("PostgreSQL connection is closed")
-#test_create_token()
-test_check_all_site_ver3()
 
-#{"isValid":false,"errorCode":117,"Error":"ERROR_UNABLE_PARSE_TREE_MESSAGE","method":"albumthemes3","payload":null}
+test_check_all_site_ver3_is_valid()
