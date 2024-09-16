515ed3981854916777233180a885b09c591d1593
pini <pinim@lupa.co.il>
2024-09-16 13:50:02 +0300

    test

---
+++ Test_run_every_day/A_STOP_ORDERS.py
@@ -0,0 +1,15 @@
+import pyodbc
+
+
+def test_connect_to_db():
+    server = '104.155.49.95'
+    database = 'lupa'
+    username = 'MachineDBA'
+    password = 'Kk28!32Zx'
+    cnxn = pyodbc.connect(
+        'DRIVER={ODBC Driver 17 for SQL Server};SERVER=' + server + ';DATABASE=' + database + ';Encrypt = Optional;UID=' + username + ';PWD=' + password)
+
+    cursor = cnxn.cursor()
+    print(cursor)
+    cursor.execute("")
+    row = cursor.fetchall()
