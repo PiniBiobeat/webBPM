075dbe95eef88cd4b1a52d78aed7b63e8d8a8331
pini <pinim@lupa.co.il>
2024-09-18 15:03:39 +0300

    test

--- Test_run_every_day/A_STOP_ORDERS.py
+++ Test_run_every_day/A_STOP_ORDERS.py
@@ -1,8 +1,11 @@
-import pyodbc
 import sys
+sys.path.append(r'C:\Users\tester\PycharmProjects\pytest-lupa')
 from Test_run_every_day.B_STOP_ORDERS import *
-sys.path.append(r'C:\path\to\Test_run_every_day')
-from B_STOP_ORDERS import *
+
+
+
+
+
 def test_connect_to_db():
     result = bring_users_to_cancel()
     master_ids = join_column_values(result, 0)
@@ -28,8 +31,8 @@ def test_connect_to_db():
     
 
     
-    update lupa_square.dbo.orders_tbl set in_status = '24' where master_id in ({master_ids}) AND in_status NOT IN (22, 23, 10) AND in_status <> '24';
-    update lupa_square.dbo.order_item_tbl set in_status = '24' where master_id in ({master_ids}) AND in_status NOT IN (22, 23, 10) AND in_status <> '24';
+    update lupa_square.dbo.orders_tbl set in_status = '24' where master_id in ({master_ids}) AND in_status NOT IN (22, 23) AND in_status <> '24';
+    update lupa_square.dbo.order_item_tbl set in_status = '24' where master_id in ({master_ids}) AND in_status NOT IN (22, 23) AND in_status <> '24';
 
 
     update lupa.dbo.orders_tbl set in_status = 'Stopped' where order_customer_id in ({user_ids}) AND in_status NOT IN ('Delivered', 'Printed', 'Phone order') AND in_status <> 'Stopped'; 
