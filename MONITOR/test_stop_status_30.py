ed7010b9ce5fc7113b62fbe288f92bd5eeb5921f
ofir <ofir@lupa.co.il>
2024-10-08 17:10:49 +0300

    test

--- MONITOR/test_stop_status_30.py
+++ MONITOR/test_stop_status_30.py
@@ -1,8 +1,9 @@
 from data_base import *
+from slack import send_slack_orders
 
 
 def test_stop_status_30():
-    select_query = "select [order_id],[master_id],[tree_version],[insert_date] from [lupa_online].[dbo].[order_item_tbl] where in_status = 30  AND DATEADD(MINUTE, 5, insert_date) < GETDATE() AND DATEADD(MINUTE, 9, insert_date) > GETDATE();"
+    select_query = "select [order_id],[master_id],[tree_version],[insert_date] from [lupa_online].[dbo].[order_item_tbl] where in_status = 30  AND DATEADD(MINUTE, 0.1, insert_date) < GETDATE() AND DATEADD(MINUTE, 9, insert_date) > GETDATE();"
     data = mysql(select_query)
     print("\n")
     temp_list = []
@@ -33,3 +34,5 @@ def test_stop_status_30():
 
     if not temp_list == []:
         send_slack_orders(block)
+        delete_query = "delete from [lupa_online].[dbo].[order_item_tbl] where in_status = 30  AND DATEADD(MINUTE, 0.1, insert_date) < GETDATE() AND DATEADD(MINUTE, 9, insert_date) > GETDATE();"
+        mysql(delete_query)
--- data_base.py
+++ data_base.py
@@ -35,20 +35,3 @@ def mysql(mysql_execute):
 #     }
 
 
-
-demoapp = "https://hooks.slack.com/services/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"
-
-def send_slack_orders(block):
-    """Send a message to Slack using a webhook URL."""
-    paymentv4 = "https://hooks.slack.com/services/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"
-
-    payload = {
-        "blocks": block,
-
-    }
-
-    try:
-        response = requests.post(paymentv4, json=payload)
-        response.raise_for_status()
-    except requests.exceptions.RequestException as e:
-        print(f"Error sending message to Slack: {e}")
