5de9463f127358c603d8582d1631efe2ff5d0172
ofir <ofir@lupa.co.il>
2024-10-13 15:54:02 +0300

    All DATA-BASE Function

--- data_base.py
+++ data_base.py
@@ -1,8 +1,120 @@
 import json
+import os
+from datetime import time
+import psycopg2
 import pyodbc
 import requests
+from infra.config.config_provider import configuration
 
 
+
+#MySQL database
+def mysql(mysql_execute):
+    server = '104.155.49.95'
+    database = 'lupa'
+    username = 'MachineDBA'
+    password = 'Kk28!32Zx'
+    cnxn = pyodbc.connect(
+        'DRIVER={ODBC Driver 17 for SQL Server};SERVER=' + server +
+        ';DATABASE=' + database +
+        ';Encrypt=No;UID=' + username +
+        ';PWD=' + password)
+
+    cursor = cnxn.cursor()
+    query1 = mysql_execute
+    cursor.execute(query1)
+    if query1.strip().lower().startswith('select'):
+        result1 = cursor.fetchall()
+    else:
+        result1 = cursor.rowcount
+    cursor.close()
+    cnxn.commit()
+    return result1
+
+
+
+
+#Postgres database: PaymentV4 / calendar
+def postgres14(postgres_execute="select",database="Tariff"):
+    connection = psycopg2.connect(user="machineDBA",
+                                  password="A#214Fdse!35dDC214XAzRDA12^79",
+                                  host="35.187.190.6",
+                                  port="5432",
+                                  database=database)
+    cursor = connection.cursor()
+    query2 = postgres_execute
+    cursor.execute(query2)
+    if query2.strip().lower().startswith('select'):
+        result2 = cursor.fetchall()
+    else:
+        result2 = cursor.rowcount
+        print(f"{result2} rows affected")
+    connection.commit()
+    cursor.close()
+    return result2
+
+
+
+
+#Postgres database: Groupa / App V3
+def groupa(groupa_execute):
+    connection = psycopg2.connect(user="machineDBA",
+                                  password="A#214Fdse!35dDC214XAzRDA12^79",
+                                  host="35.233.19.13",
+                                  port="5432",
+                                  database="groupa")
+    cursor = connection.cursor()
+    query3 = groupa_execute
+    cursor.execute(query3)
+    if query3.strip().lower().startswith('select'):
+        result3 = cursor.fetchall()
+    else:
+        result3 = cursor.rowcount
+        print(f"{result3} rows affected")
+    connection.commit()
+    cursor.close()
+    return result3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# without if to select/upate/delete
 # def mysql(mysql_execute):
 #     server = '104.155.49.95'
 #     database = 'lupa'
@@ -18,30 +130,4 @@ import requests
 #     cursor.close()
 #     cnxn.commit()
 #     return result1
-
-
-def mysql(mysql_execute):
-    server = '104.155.49.95'
-    database = 'lupa'
-    username = 'MachineDBA'
-    password = 'Kk28!32Zx'
-    cnxn = pyodbc.connect(
-        'DRIVER={ODBC Driver 17 for SQL Server};SERVER=' + server +
-        ';DATABASE=' + database +
-        ';Encrypt=No;UID=' + username +
-        ';PWD=' + password)
-
-    cursor = cnxn.cursor()
-    query1 = mysql_execute
-    cursor.execute(query1)
-    # בודקים אם השאילתה היא SELECT
-    if query1.strip().lower().startswith('select'):
-        result1 = cursor.fetchall()  # מחזירים תוצאות רק עבור SELECT
-    else:
-        result1 = None  # עבור UPDATE/DELETE לא מחזירים תוצאה
-
-    cursor.close()
-    cnxn.commit()
-    return result1
-
 
