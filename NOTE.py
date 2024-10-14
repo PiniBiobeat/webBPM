af2f1349753efe8ec1e81361be6165640bec4b9a
ofir <ofir@lupa.co.il>
2024-10-14 12:59:53 +0300

    test

--- NOTE.py
+++ NOTE.py
@@ -27,3 +27,45 @@ def test_run(playwright: Playwright) -> None:
     # ---------------------
     context.close()
     browser.close()
+
+
+
+
+
+import os
+import pyautogui
+import time
+
+
+
+import subprocess
+
+
+def git_and_update_monitor():
+    server = "146.148.2.80"
+    username = "lupa"
+    password = "A12bc28!"
+    remote_path = r"C:\Users\lupa\Desktop\powergit.ps1"
+
+    ps_command = f"""
+    $username = '{username}'
+    $password = ConvertTo-SecureString '{password}' -AsPlainText -Force
+    $cred = New-Object System.Management.Automation.PSCredential($username, $password)
+
+    $session = New-PSSession -ComputerName {server} -Credential $cred
+    Invoke-Command -Session $session -ScriptBlock {{ Start-Process "{remote_path}" }}
+    Remove-PSSession $session
+    """
+
+    # הרצת הפקודה
+    process = subprocess.Popen(["powershell.exe", ps_command], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+
+    # הדפסת פלט אם יש
+    if stdout:
+        print(stdout.decode())
+    if stderr:
+        print(stderr.decode())
+
+
+git_and_update_monitor()
