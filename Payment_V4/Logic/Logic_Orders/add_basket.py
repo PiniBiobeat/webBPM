f75287b290829177ad335939e0cef99b59114613
ofir <ofir@lupa.co.il>
2024-11-03 17:23:18 +0200

    test

---
+++ Payment_V4/Logic/Logic_Orders/add_basket.py
@@ -0,0 +1,26 @@
+
+
+
+class Add_basket:
+    def add_item(self):
+        add_basket = {
+            "tiles_square_20_blackframe": self.function(),
+            "tiles_square_30_blackframe": None,
+            "tiles_square_20_kappah": None,
+            "tiles_square_30_kappah": None,
+            "calendar_A5": None,
+            "calendar_A4": None,
+            "calendar_A3": None,
+            "v3_format_35": None,
+            "v3_format_27": None,
+            "v3_format_26": None,
+            "v3_format_6": None,
+            "v3_format_38": None,
+            "V2_format_35": None,
+            "V2_format_27": None,
+
+        }
+
+def test_l_first_item_function(self):
+        add_basket = self.add_item()
+        first_item_value = add_basket["tiles_square_20_blackframe"]
