9c7db2d167a1b7f4a6b424a12836cecbffead3f8
pini <pinim@lupa.co.il>
2024-11-27 09:26:28 +0200

    add test add to payment
    add test add to payment

---
+++ tests/TestOnline/test_add_book_to_payment.py
@@ -0,0 +1,52 @@
+import time
+
+import pytest
+from logic.pages.choose_format_cover_page import ChooseFormatCoverPage
+from tests.TestOnline.test_base_online import TestBaseOnline
+from infra.config.config_provider import configuration
+from logic.pages.login_online_page import LogInOnline
+from infra.generic_helpers import generate_random_email_and_password
+from logic.pages.connect_create_user_page import ConnectCreateUserPage
+
+text_user_name_instagram = "pinitesttiles"
+text_password_instagram = "Pinimari2020!"
+text_googleUserName="lupadevtest@gmail.com"
+text_googlePassword="lupadevtest!128"
+Short_name = "B"
+name_book =  "Book Automation"
+firt_name = "פיני"
+last_name = "mari"
+rand_email = generate_random_email_and_password()
+email =  rand_email["email"]
+password = rand_email["password"]
+num_and_pass =  password + "1"+ "!"
+email1 = 'automation@lupa.co.il'
+num_phone = '0537608055'
+firt_name1 = "פיני1"
+last_name1 = "mari1"
+email2 = "pinilupa.co.il"
+pass1 = "a123123"
+pass_not_same = "pinim123"
+
+
+
+class TestAddBookToPayment(TestBaseOnline):
+
+    @pytest.mark.smoke
+    @pytest.mark.usefixtures("before_after_test")
+    def test_add_book_to_payment_from_mybooks(self):
+        page: ConnectCreateUserPage = self.browser_online.navigate(configuration['online_url'], ConnectCreateUserPage)
+        page.click_icon_user()
+        page.click_connect_user()
+
+        page: LogInOnline = self.browser_online.create_page(LogInOnline)
+        page.insert_user_and_pass(email1, pass1)
+        page.click_login_button()
+
+        page: ConnectCreateUserPage = self.browser_online.create_page(ConnectCreateUserPage)
+        page.click_my_book_name()
+        page.wait_for_mybooks()
+        page.click_add_book_to_payment()
+
+
+        print('ss')
---
+++ tests/TestPayment/PaymantGenericFunctions.py
@@ -0,0 +1,38 @@
+from tests.TestTiles.test_base import TestBase
+from infra.config.config_provider import configuration
+from logic.pages.calendar_home_page import CalendarPage
+from logic.pages.login_calendar_page import LoginCalendarPage
+from tests.TestTiles.test_add_tiles_to_payment import TestTilesToPayment
+import os
+
+email1 = 'automation@lupa.co.il'
+pass1 = "a123123"
+master_id = 3189204
+
+images_base_path = "C:\\Users\\tester\\Desktop\\repositories\\pytest-lupa\\tests\\TestCalendar\\image_london\\"
+#path_images = result = [f"{images_base_path} ({i}).jpg" for i in range(1, 25)]
+path_images = ["./image_london/test1.jpg","./image_london/test2.jpg","./image_london/test3.jpg"]
+
+
+
+
+class PaymentPagesManager(TestBase):
+
+    def openLogin(self):
+        page: CalendarPage = self.browser.navigate(configuration['calendar_url_'+os.getenv('env')], CalendarPage)
+        page.open_menu()
+        page.open_screen_login_from_menu()
+
+        page: LoginCalendarPage = self.browser.create_page(LoginCalendarPage)
+        page.insert_user_and_pass(email1, pass1)
+        page.click_login_button()
+
+    def choose_prod(self, num):
+        match num:
+            case 1:
+                TestTilesToPayment.test_add_tiles_to_payment()
+            case 2:
+                print("You chose product 2.")
+                # Add logic for product 2
+            case _:
+                print("Invalid product selection.")
