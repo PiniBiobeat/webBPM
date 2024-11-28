04afecd89d536d81c59becdbde0b8e70ba93a85f
ofir <ofir@lupa.co.il>
2024-11-28 13:52:40 +0200

    fix

--- Payment_V4/Payment_site/Test_Pages/test_D_summary.py
+++ Payment_V4/Payment_site/Test_Pages/test_D_summary.py
@@ -3,6 +3,8 @@ from playwright.sync_api import Page
 from Payment_V4.Payment_site.Pages._General_function import Generalfunction
 from Payment_V4.Payment_site.Test_Pages.test_C_personalDetails import TestPersonalDetails
 from Payment_V4.Payment_site.Pages.D_summary import Summary
+from tests.TestPayment.test_add_book_V3 import TestAddBookV3
+
 
 @pytest.fixture
 def page(request) -> Page:
@@ -12,5 +14,6 @@ def page(request) -> Page:
 class TestSummary:
 
     def test_add_coupon(self, page):
+        TestAddBookV3().test_api_request(page)
         TestPersonalDetails().test_filler_detail(page)
         Summary(page).add_coupon("AlbumTest1")
--- tests/TestPayment/test_add_book_V3.py
+++ tests/TestPayment/test_add_book_V3.py
@@ -1,30 +1,24 @@
 import time
 
 import pytest
-from playwright.sync_api import sync_playwright
+from playwright.sync_api import Page
 from infra.generic_helpers import sql_updade_status_in_order_V3
 
 
-class TestAddBookV3():
+class TestAddBookV3:
     @pytest.mark.smoke
-    def test_api_request(self):
-        with sync_playwright() as playwright:
-            # Create a new API request context
-            request_context = playwright.request.new_context()
-            token = "be2b30408f834a74a66fbb863c4e589e"
-            # Send a GET request
-            response = request_context.get(f"https://payment-v4.lupa.co/api.aspx?method=add_basket&event_token={token}&eventtype=REGULAR&format=6&cover_type=0&page_type=5&theme=fish&pages=50&quantity=1&platform=app&source_type=books&source_device=mobile&flow=v4&lang=he&app_version=3.5.42.tf&device_type=ios&cloudcode=test&isCustomErr=false&token=FbQp8hnGcJy_k7JyihCpmPJtpzU2UHFeHT8QMQLsvTTKXe3Yph3DKeF710gy8Ultx4ZHAR44suPzBM0B_SdDJkioNGoE8BbM_0ek7-3ribMFYzf2zH8zddq-cwHgflGXNnITG4kno9vwM_y4kWeJE3RLWDb86DTR7F0TpLJyXjzwKtJdsUL_s1FJfHoPXLg-4Hy1NPkCHkNyTjhVjdrS40cebe1bypRUKVh8PHAkVl7epQPwrIo4bNwB9FLHAdLXXrTs5RcLPAPs6SXGMn88TvOqrq8Pw97NXzgoVOTX7RbHg06OcsQSmQHE11JcZSX-i9RDo6LQBN2blKZegZtnlg2")
-            assert response.ok  # Verify response is successful (status 200-299)
-            assert response.status == 200  # Verify HTTP status code
+    def test_api_request(self, page):
 
-            # Parse JSON response
-            # json_data = response.json()
-            # assert json_data["id"] == 1  # Verify expected data in response
+
+            token = "be2b30408f834a74a66fbb863c4e589e"
+            response = page.context.request.get(f"https://payment-v4.lupa.co/api.aspx?method=add_basket&event_token={token}&eventtype=REGULAR&format=6&cover_type=0&page_type=5&theme=fish&pages=50&quantity=1&platform=app&source_type=books&source_device=mobile&flow=v4&lang=he&app_version=3.5.42.tf&device_type=ios&cloudcode=test&isCustomErr=false&token=FbQp8hnGcJy_k7JyihCpmPJtpzU2UHFeHT8QMQLsvTTKXe3Yph3DKeF710gy8Ultx4ZHAR44suPzBM0B_SdDJkioNGoE8BbM_0ek7-3ribMFYzf2zH8zddq-cwHgflGXNnITG4kno9vwM_y4kWeJE3RLWDb86DTR7F0TpLJyXjzwKtJdsUL_s1FJfHoPXLg-4Hy1NPkCHkNyTjhVjdrS40cebe1bypRUKVh8PHAkVl7epQPwrIo4bNwB9FLHAdLXXrTs5RcLPAPs6SXGMn88TvOqrq8Pw97NXzgoVOTX7RbHg06OcsQSmQHE11JcZSX-i9RDo6LQBN2blKZegZtnlg2")
+            assert response.ok
+            assert response.status == 200
+
             time.sleep(5)
             sql_updade_status_in_order_V3()
 
-            # Close the request context
-            request_context.dispose()
+            page.context.close()
 
 
 
