854d56cc0ca0a9da64615b29750a3d78dda7565b
ofir <ofir@lupa.co.il>
2024-12-02 17:31:58 +0200

    fix-credit and pay

--- Payment_V4/Logic/Test_Orders/test_order_app.py
+++ Payment_V4/Logic/Test_Orders/test_order_app.py
@@ -1,11 +1,13 @@
 from playwright.sync_api import Page
 import pytest
+
 from tests.TestPayment.test_add_book_V3 import AddBookV3
 from Payment_V4.Payment_site.Pages._General_function import Generalfunction
 from Payment_V4.Payment_site.Pages.A_basket_items import BasketItems
 from Payment_V4.Payment_site.Pages.B_shipping import Shipping
 from Payment_V4.Payment_site.Pages.C_personalDetails import PersonalDetails
 from Payment_V4.Payment_site.Pages.D_summary import Summary
+from Payment_V4.Payment_site.Pages.E_creditGuard import CreditGuard
 
 from Payment_V4.Logic.Logic_Orders.copuns_album import copun_albums
 
@@ -19,14 +21,16 @@ class TestAppCoupon:
 
 
     def test_order_app_f35_hard(self, page):
-        AddBookV3().api_request(page, "הגדה פורמט 6 קלאסי פלוס")
+        # AddBookV3().api_request(page, "הגדה פורמט 6 קלאסי פלוס")
         Generalfunction(page).navigate("payment_url_books")
         BasketItems(page).valid_image_item()
         Shipping(page).home()
         PersonalDetails(page).filler_detail()
-        Summary(page).add_coupon("12930")
+        # Summary(page).add_coupon("12930")
         Summary(page).checkout()
+        CreditGuard(page).fill_credit_card().to_pay()
         page.pause()
+
 
 
     @pytest.mark.parametrize("coupon_code", copun_albums.values())
