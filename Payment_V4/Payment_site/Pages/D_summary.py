9114d733a0bcf09bea33278028b361100bf8f5b6
ofir <ofir@lupa.co.il>
2024-12-04 18:30:10 +0200

    fix

--- Payment_V4/Payment_site/Pages/D_summary.py
+++ Payment_V4/Payment_site/Pages/D_summary.py
@@ -43,7 +43,7 @@ class Summary:
 
     def checkouts(self):
         try:
-            total_discount = float(self.page.locator(self.total_discount).inner_text().replace("₪", "").replace("(", "").replace(")", ""))
+            total_discount = float(self.page.locator(self.total_discount).inner_text(timeout=1000).replace("₪", "").replace("(", "").replace(")", ""))
         except Exception: total_discount = 0
         item_count = int(self.page.locator(self.item_count).inner_text().replace("₪", ""))
         base_price = float(self.page.locator(self.base_price).inner_text().replace("₪", ""))
