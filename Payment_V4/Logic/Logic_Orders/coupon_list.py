6e4a6d422af7a46d3a355da3eef139ac73e6368d
ofir <ofir@lupa.co.il>
2024-12-05 19:45:56 +0200

    fix

--- Payment_V4/Logic/Logic_Orders/coupon_list.py
+++ Payment_V4/Logic/Logic_Orders/coupon_list.py
@@ -11,7 +11,13 @@ def get_coupon(coupon_name):
         else:
             return coupon_name
     except:
-        return print(f"more codes for {coupon_name}")
+        return coupon_name
+
+
+
+coupon_test = [
+    "AlbumTest3",
+]
 
 
 coupon_albums = [
@@ -29,42 +35,22 @@ coupon_albums = [
     "AlbumTest"
 ]
 
-coupon = [
+coupon_calendar = [
+    "AlbumShip",
+    "Album-Lior",
+    "Album-isof",
+    "AlbumFormat",
+    "AlbumBundle",
+    "AlbumTest6",
+    "AlbumTest5",
+    "AlbumTest4",
     "AlbumTest3",
+    "AlbumTest2",
+    "AlbumTest1",
+    "AlbumTest"
 ]
 
 
-@pytest.mark.parametrize("coupon_code", coupon)
+@pytest.mark.parametrize("coupon_code", coupon_albums)
 def test_list(coupon_code):
     print(get_coupon(coupon_code))
-
-
-coupon_calendar = {
-    "AlbumShip": "AlbumShip",
-    "Album-Lior": "Album-Lior",
-    "Album-isof": "Album-isof",
-    "AlbumFormat": "AlbumFormat",
-    "AlbumBundle": "AlbumBundle",
-    "AlbumTest6": "AlbumTest6",
-    "AlbumTest5": "AlbumTest5",
-    "AlbumTest4": "AlbumTest4",
-    "AlbumTest3": "AlbumTest3",
-    "AlbumTest2": "AlbumTest2",
-    "AlbumTest1": "AlbumTest1",
-    "AlbumTest": "SELECT x.* FROM cpn.coupon_tbl x WHERE name = 'AlbumTest' and use_date is null"
-}
-
-coupon_tiles = {
-    "AlbumShip": "AlbumShip",
-    "Album-Lior": "Album-Lior",
-    "Album-isof": "Album-isof",
-    "AlbumFormat": "AlbumFormat",
-    "AlbumBundle": "AlbumBundle",
-    "AlbumTest6": "AlbumTest6",
-    "AlbumTest5": "AlbumTest5",
-    "AlbumTest4": "AlbumTest4",
-    "AlbumTest3": "AlbumTest3",
-    "AlbumTest2": "AlbumTest2",
-    "AlbumTest1": "AlbumTest1",
-    "AlbumTest": "SELECT x.* FROM cpn.coupon_tbl x WHERE name = 'AlbumTest' and use_date is null"
-}
