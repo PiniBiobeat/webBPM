55b720e10ef0bbb61ba10a19278c69356937f769
ofir <ofir@lupa.co.il>
2024-12-08 17:23:51 +0200

    fix

--- Payment_V4/Logic/Logic_Orders/coupon_list.py
+++ Payment_V4/Logic/Logic_Orders/coupon_list.py
@@ -15,6 +15,31 @@ def get_coupon(coupon_name):
 
 
 
+#album sanity
+coupon_albums_shipping = [
+    "AlbumShipNamePoint",
+    "AlbumShipNamePost",
+    "AlbumShipNameHome",
+    "AlbumShipCodePoint",
+    "AlbumShipCodePost",
+    "AlbumShipCodeHome"
+]
+
+coupon_albums_isof = [
+    "Album-isof-Name",
+    "Album-isof-Code"
+]
+
+coupon_pay_for_40 = [
+    "PayFor40_Code",
+    "PayFor40_Name"
+]
+
+coupon_albums_items = [
+    "AlbumTest9_items_code"
+    "AlbumTest8_items",
+    "AlbumTest7_items",
+]
 
 coupon_albums = [
     "Album-All-Code",
@@ -28,31 +53,79 @@ coupon_albums = [
     "AlbumTest"
 ]
 
-coupon_albums_items = [
-    "AlbumTest4",
-    "AlbumTest5",
-    "AlbumTest6"
+
+
+#Calendar sanity
+coupon_calendars_shipping = [
+    "CalendarShipNamePoint",
+    "CalendarShipNamePost",
+    "CalendarShipNameHome",
+    "CalendarShipCodePoint",
+    "CalendarShipCodePost",
+    "CalendarShipCodeHome"
+]
+
+coupon_calendars_isof = [
+    "Calendar-isof-Name",
+    "Calendar-isof-Code"
+]
+
+coupon_calendars_items = [
+    "CalendarTest9_items_code"
+    "CalendarTest8_items",
+    "CalendarTest7_items",
+]
+
+coupon_calendars = [
+    "Calendar-All-Code",
+    "Calendar-All-Name",
+    "CalendarTest6",
+    "CalendarTest5_Code",
+    "CalendarTest4",
+    "CalendarTest3_Code",
+    "CalendarTest2",
+    "CalendarTest1_Code",
+    "CalendarTest"
 ]
 
 
-coupon_albums_shipping = [
-    "AlbumShipNamePoint",
-    "AlbumShipNamePost",
-    "AlbumShipNameHome",
-    "AlbumShipCodePoint",
-    "AlbumShipCodePost",
-    "AlbumShipCodeHome"
 
+
+#Tiles sanity
+coupon_tiles_shipping = [
+    "TilesShipNamePoint",
+    "TilesShipNamePost",
+    "TilesShipNameHome",
+    "TilesShipCodePoint",
+    "TilesShipCodePost",
+    "TilesShipCodeHome"
 ]
 
-coupon_albums_isof = [
-    "Album-isof-Name",
-    "Album-isof-Code"
+coupon_tiles_isof = [
+    "Tiles-isof-Name",
+    "Tiles-isof-Code"
 ]
-coupon_pay_for_40 = [
-    "PayFor40_Code",
-    "PayFor40_Name"
+
+coupon_tiles_items = [
+    "TilesTest9_items_code"
+    "TilesTest8_items",
+    "TilesTest7_items",
 ]
+
+coupon_tiles = [
+    "Tiles-All-Code",
+    "Tiles-All-Name",
+    "TilesTest6",
+    "TilesTest5_Code",
+    "TilesTest4",
+    "TilesTest3_Code",
+    "TilesTest2",
+    "TilesTest1_Code",
+    "TilesTest"
+]
+
+
+
 
 
 
