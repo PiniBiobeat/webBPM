e9f8434d99a995681c9bc27d7c7cf406cbacac31
ofir <ofir@lupa.co.il>
2024-12-10 13:01:28 +0200

    fix

--- Payment_V4/Logic/Test_Orders/test_order_online.py
+++ Payment_V4/Logic/Test_Orders/test_order_online.py
@@ -22,12 +22,12 @@ def page(request) -> Page:
 class TestOnlineCoupon:
 
     def test_order_online_f35(self, page):
-        AddBookOnline().api_request_online(page, "פורמט_35_ריבועי_גדול_הולנדי")
+        AddBookOnline().api_request_online(page, "פורמט_35_ריבועי_גדול_קשה")
         Generalfunction(page).navigate("payment_url_books")
         BasketItems(page).valid_element_click_next()
         Shipping(page).asafta()
         PersonalDetails(page).filler_detail()
-        Summary(page).add_coupon(get_coupon("AlbumTest")).checkouts()
+        Summary(page).checkouts()
         CreditGuard(page).fill_credit_card().to_pay()
         Thanks(page).status()
 
