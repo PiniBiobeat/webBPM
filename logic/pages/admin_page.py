e2b7b7b6a8355a49c78e6d40c79a95a1715feb49
pini <pinim@lupa.co.il>
2024-12-26 09:29:26 +0200

    fix test
    fix test

--- logic/pages/admin_page.py
+++ logic/pages/admin_page.py
@@ -29,23 +29,18 @@ class AdminPage(PageBase):
         self.pw_page.locator(self.text_user_login).fill(text_user)
         self.pw_page.locator(self.test_pass_login).fill(text_pass)
 
-    def click_login_button(self):
+    def click_login_button(self,order_id):
         self.pw_page.click(self.text_button_login)
-        details_url = self.pw_page.url.replace("order_list.aspx", "Order_Details.aspx?id=7823989")
+        details_url = self.pw_page.url.replace("order_list.aspx", f"Order_Details.aspx?id={order_id}")
         return details_url
 
     def add_num_sale(self, num_coupons):
         self.pw_page.locator(self.text_num_coupons).fill(str(num_coupons))
-
-    def select_option(self):
         self.pw_page.locator(self.test_open_option).select_option(value="הטבה שירותית")
-
-    def click_ok(self):
         self.pw_page.click(self.text_click_ok)
 
     def click_open_link(self):
         self.pw_page.once("dialog", lambda dialog: dialog.accept())
-        print("")
 
         def handle_dialog(dialog):
             print(f"Dialog message: {dialog.message}")
@@ -60,15 +55,26 @@ class AdminPage(PageBase):
         new_page = new_page_info.value
         print(new_page)
 
-    def get_url_from_new_page(self):
-        self.pw_page.wait_for_load_state()
-        self.pw_page.goto("https://admin.lupa.co.il/admin_online/ajax/sendPaymentToCustomer.aspx?orderid=7823989")
-        body_text = self.pw_page.locator("body").text_content()
-        self.pw_page.goto(body_text)
-        self.pw_page.wait_for_load_state()
-        print(body_text)
-        #
-        # return body_text
+    def get_url_from_new_page(self,order_id):
+
+            # Navigate to the initial URL
+            url = f"https://admin.lupa.co.il/admin_online/ajax/sendPaymentToCustomer.aspx?orderid={order_id}"
+            self.pw_page.goto(url)
+
+            # Wait for the body locator to be visible
+            body_locator = self.pw_page.locator("body")
+            body_locator.wait_for(state="visible")
+
+            # Retrieve the body text
+            body_text = body_locator.text_content()
+
+            # Validate the extracted URL
+            if not body_text.startswith("http"):
+                raise ValueError(f"Invalid URL found in body text: {body_text}")
+
+            # Navigate to the extracted URL
+            self.pw_page.goto(body_text)
+
 
     def pay_order(self, card="5451365000064667", year="2030", month="01", cvv="973"):
         self.pw_page.frame_locator(self.iframe).locator(self.credit_num).fill(card)
