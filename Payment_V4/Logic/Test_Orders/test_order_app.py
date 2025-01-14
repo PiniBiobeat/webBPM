2d035dd3f5fa0a17fb468fdded4eaa1ba1e3b43f
ofir <ofir@lupa.co.il>
2025-01-14 16:49:15 +0200

    FIX

--- Payment_V4/Logic/Test_Orders/test_order_app.py
+++ Payment_V4/Logic/Test_Orders/test_order_app.py
@@ -29,7 +29,7 @@ class TestAppCouponSanity:
         BasketItems(page).valid_element_click_next()
         Shipping(page).asafta()
         PersonalDetails(page).filler_detail()
-        Summary(page).add_coupon("NONE")
+        Summary(page).add_coupon("AlbumFormat")
         page.pause()
         Summary(page).checkouts()
         CreditGuard(page).fill_credit_card().to_pay()
