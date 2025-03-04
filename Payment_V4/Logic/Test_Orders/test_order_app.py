c77cb4a2a5367c8ecfb92ed62632daf13c432332
ofir <ofir@lupa.co.il>
2025-03-04 15:59:51 +0200

    FIX

--- Payment_V4/Logic/Test_Orders/test_order_app.py
+++ Payment_V4/Logic/Test_Orders/test_order_app.py
@@ -30,15 +30,14 @@ class TestAppCouponSanity:
         AddBookV3().requestV3(page, "פורמט_35_ריבועי_גדול_קשה")
         Generalfunction(page).navigate("payment_url_books")
         BasketItems(page).valid_element_click_next()
-        # Shipping(page).asafta()
-        # PersonalDetails(page).filler_detail()
-        # Summary(page).add_coupon("AlbumFormat")
-        # # page.pause()
-        # Summary(page).checkouts()
-        # CreditGuard(page).fill_credit_card().to_pay()
-        # Thanks(page).status()
+        Shipping(page).asafta()
+        PersonalDetails(page).filler_detail()
+        Summary(page).add_coupon("AlbumFormat")
+        # page.pause()
+        Summary(page).checkouts()
+        CreditGuard(page).fill_credit_card().to_pay()
+        Thanks(page).status()
         assert AssertOrder().general_assert_orders()
-
 
 
     @pytest.mark.parametrize("coupon_code", coupon_albums)
